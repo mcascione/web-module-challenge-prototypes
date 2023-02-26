f4775c613a2970869cfe5d057fd5de58c3083e35
Miranda Cascione <miranda.cascione@gmail.com>
2023-02-26 15:38:23 -0800

    Completed Task 1

--- index.js
+++ index.js
@@ -15,10 +15,26 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+this.name = name; 
+this.age = age;
+this.stomach = [];
 }
 
+Person.prototype.eat = function (food){
+  if (this.stomach.length < 10){
+    this.stomach.push(food);
+  }
+};
+
+Person.prototype.poop = function (){
+  if (this.stomach.length = 0){
+  }
+};
+
+Person.prototype.toString = function (){
+  return `${this.name},${this.age}`;
+};
 
 /*
   TASK 2
